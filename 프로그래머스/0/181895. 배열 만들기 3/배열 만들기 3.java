6499ec7e488138755a3ce5229b2886f64e7ad49e
rlatjddms <145992500+rlatjddms@users.noreply.github.com>
2024-10-06 23:40:29 +0900

    [level 0] Title: 배열 만들기 3, Time: 5.29 ms, Memory: 94.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public ArrayList<Integer> solution(int[] arr, int[][] intervals) {
+        ArrayList<Integer> answer = new ArrayList<>();
+        
+        for(int i=0; i<intervals.length; i++) {
+            for(int j=intervals[i][0]; j<=intervals[i][1]; j++) {
+                answer.add(arr[j]);
+            }
+        }
+        
+        return answer;
+    }
+}
