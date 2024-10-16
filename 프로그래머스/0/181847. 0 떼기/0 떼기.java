2e26d80d99fe8378be69effb326cbf6afbfd1c50
rlatjddms <145992500+rlatjddms@users.noreply.github.com>
2024-10-16 14:21:20 +0900

    [level 0] Title: 0 떼기, Time: 0.04 ms, Memory: 68.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.java
@@ -0,0 +1,8 @@
+class Solution {
+    public String solution(String n_str) {
+        String answer = "";
+        
+        answer = String.valueOf(Integer.parseInt(n_str));
+        return answer;
+    }
+}
