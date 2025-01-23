ef7f36070aedf11fb012a5e8fedaee3890fa198e
rlatjddms <145992500+rlatjddms@users.noreply.github.com>
2025-01-23 22:24:42 +0900

    [level 1] Title: 폰켓몬, Time: 3.84 ms, Memory: 74.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,19 @@
+import java.util.HashSet;
+
+public class Solution {
+    public int solution(int[] nums) {
+        int max = nums.length / 2;
+
+        HashSet<Integer> hashSet = new HashSet<>();
+
+        for (int n : nums) {
+            hashSet.add(n);
+        }
+
+        if (max >= hashSet.size()) {
+            return hashSet.size();
+        } else {
+            return max;
+        }
+    }
+}
